b34fc4556be90ada9b4daba07965f903ae1c23ff
Illustris <illustrisinteractive@gmail.com>
2021-12-21 00:38:14 +0800

    implemented reputation card

--- webapp-main/components/reputation.js
+++ webapp-main/components/reputation.js
@@ -2,6 +2,65 @@ import React, { Component } from "react";
 
 export default class Reputation extends Component {
   render() {
-    return <>Hi</>;
+    return (
+      <>
+        <div className="p-5 bg-white h-auto rounded-md shadow">
+          <div className="grid grid-cols-2">
+            <div className="col-span-1">
+              <p>Post Reputation</p>
+              <p className="font-bold font-number text-xl">0</p>
+            </div>
+            <div className="col-span-1">
+              <p>Comment Reputation</p>
+              <p className="font-bold font-number text-xl">0</p>
+            </div>
+          </div>
+          {reputationStatus()}
+        </div>
+      </>
+    );
+  }
+}
+
+function reputationStatus() {
+  const reputation = -10;
+  const partner = false;
+
+  if (partner) {
+    return (
+      <div className="p-3 bg-cyan-600 mt-3 rounded-md">
+        <p className="font-bold text-md text-white">PARTNER</p>
+        <p className="font-light text-sm text-white">
+          Together, we make a safe community.
+        </p>
+      </div>
+    );
+  } else if (reputation > 100 && !partner) {
+    return (
+      <div className="p-3 bg-emerald-600 mt-3 rounded-md">
+        <p className="font-bold text-md text-white">TRUSTED</p>
+        <p className="font-light text-sm text-white">
+          Thank you for helping keep your community safe.
+        </p>
+      </div>
+    );
+  } else if (reputation < 100 && !partner && reputation > 0) {
+    return (
+      <div className="p-3 bg-gray-500 mt-3 rounded-md">
+        <p className="font-bold text-md text-white">USER</p>
+        <p className="font-light text-sm text-white">
+          Get 100 total reputation to earn a Trusted badge.
+        </p>
+      </div>
+    );
+  } else if (reputation < 0 && !partner) {
+    return (
+      <div className="p-3 bg-red-700 mt-3 rounded-md">
+        <p className="font-bold text-md text-white">UNRELIABLE</p>
+        <p className="font-light text-sm text-white">
+          Get 20 total reputation to remove this badge.
+        </p>
+      </div>
+    );
   }
 }
--- webapp-main/pages/index.js
+++ webapp-main/pages/index.js
@@ -1,13 +1,14 @@
 import Head from "next/head";
+import Reputation from "../components/reputation";
 
 export default function Home() {
   return (
     <>
-      <div className="flex grid grid-cols-10 justify-center mx-4 lg:mx-32 xl:mx-52 2xl:mx-64 gap-x-4">
+      <div className="flex grid grid-cols-10 justify-center mx-8 xl:mx-52 2xl:mx-64 gap-x-4">
         <div className="col-span-2 bg-gray-600 h-screen">Sidebar pane</div>
         <div className="col-span-5 bg-gray-600 h-screen">Content pane</div>
         <div className="col-span-3 bg-gray-600 h-screen">
-          Reputation/Auxillary Pane
+          <Reputation />
         </div>
       </div>
     </>
