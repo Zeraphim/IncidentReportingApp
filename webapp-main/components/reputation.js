30b9671bc16e4298f6c737c347abd7b54e9e1f44
Illustris <illustrisinteractive@gmail.com>
2021-12-21 18:09:11 +0800

    commit

--- webapp-main/components/reputation.js
+++ webapp-main/components/reputation.js
@@ -4,15 +4,15 @@ export default class Reputation extends Component {
   render() {
     return (
       <>
-        <div className="p-5 bg-white h-auto rounded-md shadow">
-          <div className="grid grid-cols-2">
+        <div className="p-5 bg-gray-100 h-auto rounded-md shadow invisible lg:visible">
+          <div className="grid grid-cols-2 mb-3">
             <div className="col-span-1">
-              <p>Post Reputation</p>
-              <p className="font-bold font-number text-xl">0</p>
+              <p className="text-sm 2xl:text-lg">Post Reputation</p>
+              <p className="font-bold font-number text-2xl">0</p>
             </div>
             <div className="col-span-1">
-              <p>Comment Reputation</p>
-              <p className="font-bold font-number text-xl">0</p>
+              <p className="text-sm 2xl:text-lg">Comment Reputation</p>
+              <p className="font-bold font-number text-2xl">0</p>
             </div>
           </div>
           {reputationStatus()}
@@ -23,12 +23,13 @@ export default class Reputation extends Component {
 }
 
 function reputationStatus() {
-  const reputation = -10;
+  const reputation = 400;
   const partner = false;
+  let result = <></>;
 
   if (partner) {
-    return (
-      <div className="p-3 bg-cyan-600 mt-3 rounded-md">
+    result = (
+      <div className="p-3 bg-cyan-600 rounded-md">
         <p className="font-bold text-md text-white">PARTNER</p>
         <p className="font-light text-sm text-white">
           Together, we make a safe community.
@@ -36,8 +37,8 @@ function reputationStatus() {
       </div>
     );
   } else if (reputation > 100 && !partner) {
-    return (
-      <div className="p-3 bg-emerald-600 mt-3 rounded-md">
+    result = (
+      <div className="p-3 bg-emerald-600 rounded-md">
         <p className="font-bold text-md text-white">TRUSTED</p>
         <p className="font-light text-sm text-white">
           Thank you for helping keep your community safe.
@@ -45,8 +46,8 @@ function reputationStatus() {
       </div>
     );
   } else if (reputation < 100 && !partner && reputation > 0) {
-    return (
-      <div className="p-3 bg-gray-500 mt-3 rounded-md">
+    result = (
+      <div className="p-3 bg-gray-500 rounded-md">
         <p className="font-bold text-md text-white">USER</p>
         <p className="font-light text-sm text-white">
           Get 100 total reputation to earn a Trusted badge.
@@ -54,8 +55,8 @@ function reputationStatus() {
       </div>
     );
   } else if (reputation < 0 && !partner) {
-    return (
-      <div className="p-3 bg-red-700 mt-3 rounded-md">
+    result = (
+      <div className="p-3 bg-red-700 rounded-md">
         <p className="font-bold text-md text-white">UNRELIABLE</p>
         <p className="font-light text-sm text-white">
           Get 20 total reputation to remove this badge.
@@ -63,4 +64,6 @@ function reputationStatus() {
       </div>
     );
   }
+
+  return result;
 }
--- webapp-main/pages/index.js
+++ webapp-main/pages/index.js
@@ -1,13 +1,18 @@
 import Head from "next/head";
 import Reputation from "../components/reputation";
+import Sidebar from "../components/sidebar";
 
 export default function Home() {
   return (
     <>
-      <div className="flex grid grid-cols-10 justify-center mx-8 xl:mx-52 2xl:mx-64 gap-x-4">
-        <div className="col-span-2 bg-gray-600 h-screen">Sidebar pane</div>
-        <div className="col-span-5 bg-gray-600 h-screen">Content pane</div>
-        <div className="col-span-3 bg-gray-600 h-screen">
+      <div className="flex grid grid-cols-10 justify-center mx-8 xl:mx-16 2xl:mx-64 gap-x-4">
+        <div className="col-span-2 bg-gray-600 h-screen hidden lg:block">
+          <Sidebar />
+        </div>
+        <div className="bg-gray-600 h-screen col-span-10 lg:col-span-5">
+          Content pane
+        </div>
+        <div className="col-span-3 bg-gray-600 h-screen hidden lg:block">
           <Reputation />
         </div>
       </div>
