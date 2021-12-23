425c4c73e4b25feafc77e187a8057f3ea814a570
Illustris <illustrisinteractive@gmail.com>
2021-12-24 02:47:59 +0800

    begun implementation of new post modal

--- webapp-main/components/reputation.js
+++ webapp-main/components/reputation.js
@@ -4,7 +4,7 @@ export default class Reputation extends Component {
   render() {
     return (
       <>
-        <div className="p-5 bg-gray-100 h-auto rounded-md shadow invisible lg:visible">
+        <div className="p-5 bg-gray-100 h-auto rounded-md shadow invisible lg:visible select-none">
           <div className="grid grid-cols-2 mb-3">
             <div className="col-span-1">
               <p className="text-sm 2xl:text-md">Post Reputation</p>
@@ -23,7 +23,7 @@ export default class Reputation extends Component {
 }
 
 function reputationStatus() {
-  const reputation = 400;
+  const reputation = 410;
   const partner = false;
   let result = <></>;
 
@@ -31,7 +31,7 @@ function reputationStatus() {
     result = (
       <div className="p-3 bg-cyan-600 rounded-md">
         <p className="font-bold text-md text-white">PARTNER</p>
-        <p className="font-light text-sm text-white">
+        <p className="font-light text-sm text-white hidden 2xl:block">
           Together, we make a safe community.
         </p>
       </div>
@@ -40,7 +40,7 @@ function reputationStatus() {
     result = (
       <div className="p-3 bg-emerald-600 rounded-md">
         <p className="font-bold text-md text-white">TRUSTED</p>
-        <p className="font-light text-sm text-white">
+        <p className="font-light text-sm text-white hidden 2xl:block">
           Thank you for helping keep your community safe.
         </p>
       </div>
@@ -48,8 +48,8 @@ function reputationStatus() {
   } else if (reputation < 100 && !partner && reputation > 0) {
     result = (
       <div className="p-3 bg-gray-500 rounded-md">
-        <p className="font-bold text-md text-white">USER</p>
-        <p className="font-light text-sm text-white">
+        <p className="font-bold text-md text-white ">USER</p>
+        <p className="font-light text-sm text-white hidden 2xl:block">
           Get 100 total reputation to earn a Trusted badge.
         </p>
       </div>
@@ -57,8 +57,8 @@ function reputationStatus() {
   } else if (reputation < 0 && !partner) {
     result = (
       <div className="p-3 bg-red-700 rounded-md">
-        <p className="font-bold text-md text-white">UNRELIABLE</p>
-        <p className="font-light text-sm text-white">
+        <p className="font-bold text-md text-white ">UNRELIABLE</p>
+        <p className="font-light text-sm text-white hidden 2xl:block">
           Get 20 total reputation to remove this badge.
         </p>
       </div>
